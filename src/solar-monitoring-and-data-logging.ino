48a7ad59b1f7f4f6015ebe2d27234c8cdd0dcc1a
Nur Aziz <sukabaru09@gmail.com>
2025-10-22 08:45:38 +0700

    feat: sensor calibration

--- src/solar-monitoring-and-data-logging.ino
+++ src/solar-monitoring-and-data-logging.ino
@@ -2,31 +2,27 @@
 #include <DallasTemperature.h>
 #include <LiquidCrystal_I2C.h>
 #include <OneWire.h>
+#include <PubSubClient.h>
 #include <WiFi.h>
-#include <WiFiClientSecure.h>
 #include <Wire.h>
 
 #include "ACS712.h"
 
 // ====== User config ======
-constexpr char WIFI_SSID[] = "UGM-Hotspot";
-constexpr char WIFI_PASSWORD[] = "";
+constexpr char WIFI_SSID[] = "Aziz";
+constexpr char WIFI_PASSWORD[] = "[qwaszx]";
 constexpr int WIFI_TIMEOUT_MS = 5000;
-constexpr char TOKEN[] = "ByExtlDrqCtw59F5gM6C";
-constexpr char HOST[] = "thingsboard.cloud";
-constexpr int HTTPS_PORT = 443;
+constexpr char TOKEN[] = "e0k4v737c8318cpm4rne";
+constexpr char HOST[] = "tb.crazyz.biz.id";
+constexpr int PORT = 1883;
 constexpr uint32_t SERIAL_BAUD = 115200;
-constexpr int HTTP_TIMEOUT = 3000;
-
-// ====== Wi-Fi ======
-WiFiClientSecure client;
 
 // ====== LED ======
 #define LED_PIN 2
 inline void ledOn() { digitalWrite(LED_PIN, HIGH); }
 inline void ledOff() { digitalWrite(LED_PIN, LOW); }
 
-// ====== Pins ======
+// ====== Sensor Pins ======
 constexpr int VS_PIN = 32;     // Voltage sensor
 constexpr int ACS_PIN = 33;    // ACS712 signal directly (no divider)
 constexpr int DS_PIN = 23;     // DS18B20 data (with 4.7k pull-up to 3.3V)
@@ -36,12 +32,12 @@ constexpr int RELAY_PIN = 13;  // Relay control (active HIGH)
 OneWire oneWire(DS_PIN);
 DallasTemperature DSSensor(&oneWire);
 
-// ====== Voltage sensor config ======
+// ====== Voltage sensor configuration ======
 constexpr float ADC_VREF = 3.3f;  // ESP32 ADC reference (approx.)
 constexpr int ADC_MAX = 4095;     // 12-bit
 const float R1_V = 7500.0f;       // ohms (top resistor, 7501)
 const float R2_V = 30000.0f;      // ohms (bottom resistor, 3012)
-const float VOLT_CAL = 2.9f;      // fine calibration multiplier
+const float VOLT_CAL = 4.15f;     // fine calibration multiplier
 
 // ====== ACS712 configuration ======
 //  Arduino UNO has 5.0 volt with a max ADC value of 1023 steps
@@ -51,20 +47,26 @@ const float VOLT_CAL = 2.9f;      // fine calibration multiplier
 
 // ACS712 ACS(A0, 5.0, 1023, 100);
 ACS712 ACS(ACS_PIN, 3.3, 3725, 185);
+const float CURRENT_CAL = 0.85f;  // fine calibration multiplier
 
-// ====== BH1750 light sensor ======
+// ====== BH1750 light sensor configuration ======
 BH1750 lightMeter(0x23);
 
-// ====== LCD I2C 16x2 ======
+// ====== LCD I2C 16x2 configuration ======
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 // ====== Function declarations ======
 void initWiFi();
-float readVoltage_mV();
-float readTemperatureC();
+float readVoltage();
+float readTemperature();
 float readLight();
 void sendTelemetry(float voltage_mV, float current_mA, float temperature, float light);
-void getRelayState();
+void mqttConnect();
+void callback(char* topic, byte* payload, unsigned int length);
+
+// ====== WiFi and MQTT ======
+WiFiClient espClient;
+PubSubClient client(HOST, PORT, callback, espClient);
 
 // ====== Setup & Loop ======
 unsigned long lastSend = 0;
@@ -73,15 +75,15 @@ const unsigned long SEND_PERIOD_MS = 1000;  // 1s
 void setup() {
   Wire.begin();
   pinMode(LED_PIN, OUTPUT);
+
+  // Relay & ACS712 initialization
   pinMode(RELAY_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, LOW);
+  ACS.autoMidPoint();
 
   ledOn();
   delay(500);
   ledOff();
-
-  //  assuming no current flowing at startup
-  ACS.autoMidPoint();
 
   lcd.begin(16, 2);
   lcd.backlight();
@@ -106,7 +108,6 @@ void setup() {
   initWiFi();
 
   DSSensor.begin();
-  digitalWrite(RELAY_PIN, HIGH);
 }
 
 void loop() {
@@ -115,49 +116,36 @@ void loop() {
     initWiFi();
   }
 
+  if (!client.connected()) {
+    mqttConnect();
+  }
+  client.loop();
+
   unsigned long now = millis();
   if (now - lastSend >= SEND_PERIOD_MS) {
     lastSend = now;
 
-    float voltage_mV = readVoltage_mV();     // ~0–4.1 V (7.5k/30k divider)
-    float current_mA = ACS.mA_DC();          // -5000 to +5000 mA (5A version)
-    float temperature = readTemperatureC();  // 20.0–39.9 °C (random)
-    float light = readLight();               // BH1750
+    float voltage = readVoltage();          // ~0–4.1 V (7.5k/30k divider)
+    float current = readCurrent();          // -5000 to +5000 mA (5A version)
+    float temperature = readTemperature();  // 20.0–39.9 °C (random)
+    float light = readLight();              // BH1750
 
-    Serial.printf("VIN=%.2f mV, I=%.1f mA, T=%.1f C, L=%.1f lux\n", voltage_mV, current_mA, temperature, light);
+    Serial.printf("VIN=%.2f V, I=%.3f A, T=%.1f C, L=%.1f lux\n", voltage, current, temperature, light);
 
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.printf("V:%1.2fV", voltage_mV / 1000);
+    lcd.printf("V:%1.2fV", voltage);
     lcd.setCursor(0, 1);
-    lcd.printf("I:%1.2fA", current_mA / 1000);
+    lcd.printf("I:%1.2fA", current);
 
     lcd.setCursor(9, 0);
     lcd.printf("T:%d", (int)temperature);
     lcd.print((char)223);
     lcd.print("C");
     lcd.setCursor(9, 1);
-    lcd.printf("L:%ulx", (int)light);
+    lcd.printf("L:%dlx", (int)light);
 
-    sendTelemetry(voltage_mV, current_mA, temperature, light);
-  }
-
-  getRelayState();
-
-  if (Serial.available() > 0) {
-    String data = Serial.readStringUntil('\n');
-    data.trim();
-
-    Serial.println("Received command: " + data);
-    if (data == "ON") {
-      digitalWrite(RELAY_PIN, HIGH);
-      Serial.println("Relay ON");
-    } else if (data == "OFF") {
-      digitalWrite(RELAY_PIN, LOW);
-      Serial.println("Relay OFF");
-    } else {
-      Serial.println("Unknown command");
-    }
+    sendTelemetry(voltage, current, temperature, light);
   }
 }
 
@@ -183,14 +171,10 @@ void initWiFi() {
     Serial.print(F("IP address: "));
     Serial.println(WiFi.localIP());
   }
-
-  client.setInsecure();  // ⚠️ For production, replace with client.setCACert(root_ca)
-  client.setTimeout(HTTP_TIMEOUT / 1000);
-  ledOn();
 }
 
-// Voltage via divider
-float readVoltage_mV() {
+// ===== Voltage sensor (0-25v) ======
+float readVoltage() {
   const int SAMPLES = 32;
   uint32_t sum = 0;
   for (int i = 0; i < SAMPLES; ++i) {
@@ -200,47 +184,28 @@ float readVoltage_mV() {
   float raw = sum / (float)SAMPLES;
   float vpin = (raw / ADC_MAX) * ADC_VREF;
   float vin = vpin * ((R1_V + R2_V) / R2_V) * VOLT_CAL;
-  return vin * 1000.0f;  // convert V to mV
+  return vin;
 }
 
-float readTemperatureC() {
+// ===== Current via ACS712 ======
+float readCurrent() {
+  const int SAMPLES = 32;
+  float sum = 0.0f;
+  for (int i = 0; i < SAMPLES; ++i) {
+    sum += ACS.mA_DC();
+    delayMicroseconds(120);
+  }
+
+  return sum / (float)SAMPLES / 1000.0f * CURRENT_CAL;  // Convert mA to A
+}
+
+// ===== Temperature via DS18B20 ======
+float readTemperature() {
   DSSensor.requestTemperatures();
   return DSSensor.getTempCByIndex(0);
 }
 
-// ====== Send telemetry via HTTPS ======
-void sendTelemetry(float voltage_mV, float current_mA, float temperature, float light) {
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.println(F("WiFi not connected, cannot send telemetry"));
-    return;
-  }
-
-  const long start = millis();
-  if (!client.connect(HOST, HTTPS_PORT, HTTP_TIMEOUT)) {
-    Serial.println(F("Connection to server failed!"));
-    return;
-  }
-
-  String payload = "{";
-  payload += "\"voltage\":" + String(voltage_mV, 2) + ",";
-  payload += "\"current\":" + String(current_mA, 1) + ",";  // send milliamps
-  payload += "\"temperature\":" + String(temperature, 1) + ",";
-  payload += "\"light\":" + String(light, 1);
-  payload += "}";
-
-  String url = "/api/v1/" + String(TOKEN) + "/telemetry";
-  String request = "POST " + url + " HTTP/1.1\r\n" + "Host: " + String(HOST) + "\r\n" + "Content-Type: application/json\r\n" +
-                   "Content-Length: " + String(payload.length()) + "\r\n" + "Connection: close\r\n\r\n" + payload;
-
-  client.print(request);
-
-  while (client.connected() && (millis() - start < HTTP_TIMEOUT)) {
-    String line = client.readStringUntil('\n');
-    if (line == "\r") break;
-  }
-  client.stop();
-}
-
+// ===== Light via BH1750 ======
 float readLight() {
   if (lightMeter.measurementReady()) {
     return lightMeter.readLightLevel();
@@ -248,43 +213,63 @@ float readLight() {
   return -1.0f;
 }
 
-void getRelayState() {
+// ===== MQTT connect ======
+void mqttConnect() {
+  if (!client.connected()) {
+    ledOff();
+    Serial.print("Attempting MQTT connection...");
+    String clientId = "ESP32Client-";
+    clientId += String(random(0xffff), HEX);
+    if (client.connect(clientId.c_str(), TOKEN, NULL)) {
+      Serial.println("connected");
+      client.subscribe("v1/devices/me/attributes");
+      client.subscribe("v1/devices/me/attributes/response/+");
+      client.publish("v1/devices/me/attributes/request/1", "{\"sharedKeys\":\"relay\"}");
+      ledOn();
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      delay(1000);
+    }
+  }
+}
+
+// ===== MQTT message callback ======
+void callback(char* topic, byte* payload, unsigned int length) {
+  String t = String(topic);
+  String message;
+  for (unsigned int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+  //   Serial.println("Message arrived: " + message);
+
+  int relayStart = message.indexOf("\"relay\":");
+  if (relayStart >= 0) {
+    if (message.indexOf("true", relayStart) >= 0) {
+      digitalWrite(RELAY_PIN, HIGH);
+      Serial.println("Relay set to ON from MQTT");
+    } else if (message.indexOf("false", relayStart) >= 0) {
+      digitalWrite(RELAY_PIN, LOW);
+      Serial.println("Relay set to OFF from MQTT");
+    }
+  }
+}
+
+// ====== Send sensor telemetry ======
+void sendTelemetry(float voltage, float current, float temperature, float light) {
   if (WiFi.status() != WL_CONNECTED) {
     Serial.println(F("WiFi not connected, cannot send telemetry"));
     return;
   }
 
-  if (!client.connect(HOST, HTTPS_PORT, HTTP_TIMEOUT)) {
-    Serial.println(F("Connection to server failed!"));
-    return;
-  }
+  String payload = "{";
+  payload += "\"voltage\":" + String(voltage, 2) + ",";
+  payload += "\"current\":" + String(current, 3) + ",";
+  payload += "\"temperature\":" + String(temperature, 1) + ",";
+  payload += "\"light\":" + String(light, 1);
+  payload += "}";
 
-  String url = "/api/v1/" + String(TOKEN) + "/attributes?sharedKeys=relay";
-  String request =
-      "GET " + url + " HTTP/1.1\r\n" + "Host: " + String(HOST) + "\r\n" + "Content-Type: application/json\r\n" + "Connection: close\r\n\r\n";
-  client.print(request);
-
-  String response;
-  while (client.connected()) {
-    String line = client.readStringUntil('\n');
-    if (line == "\r") break;
+  if (!client.publish("v1/devices/me/telemetry", payload.c_str())) {
+    Serial.println("Failed to send telemetry");
   }
-  while (client.available()) {
-    response += client.readString();
-  }
-  //   Serial.println("Response: " + response);
-
-  // Parse the JSON response to get relay state
-  int relayStart = response.indexOf("\"relay\":");
-  if (relayStart != -1) {
-    if (response.indexOf("true", relayStart) != -1) {
-      digitalWrite(RELAY_PIN, HIGH);
-      Serial.println("Relay set to ON from server");
-    } else if (response.indexOf("false", relayStart) != -1) {
-      digitalWrite(RELAY_PIN, LOW);
-      Serial.println("Relay set to OFF from server");
-    }
-  }
-
-  client.stop();
 }
